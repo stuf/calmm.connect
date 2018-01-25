b903c6cf5b39ca188f19077317f97c17f778cf8c
Stefan Rimaila <stefan@rimaila.fi>
2018-01-25 13:57:06 +0200

    Change rollup config to support React's named exports

--- rollup.config.js
+++ rollup.config.js
@@ -4,25 +4,35 @@ import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 import cjs from 'rollup-plugin-commonjs';
 
+const globals = {
+  'react': 'React',
+  'karet': 'karet',
+  'kefir': 'Kefir',
+  'prop-types': 'P',
+  'karet.util': 'U',
+  'ramda': 'R',
+  'partial.lenses': 'L',
+};
+
 export default {
-  external: ['ramda', 'karet', 'karet.util', 'partial.lenses'],
-  output: {
-    globals: {
-      'karet': 'React',
-      'karet.util': 'U',
-      'ramda': 'R',
-      'partial.lenses': 'L',
-    }
-  },
+  external: Object.keys(globals),
+  output: { globals },
   plugins: [
     resolve(),
     process.env.NODE_ENV &&
       replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
     babel({
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**',
     }),
-    cjs(),
-    process.env.NODE_ENV === 'production' && uglify()
-  ].filter(x => x)
+    cjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'node_modules/react/index.js': [
+          'Component',
+        ],
+      },
+    }),
+    process.env.NODE_ENV === 'production' && uglify(),
+  ].filter(x => x),
 };
 
